154ae03752216505d8222d3255c8e72e275c4e94
Ryan Tablada <ryan.tablada@gmail.com>
2016-06-10 16:21:37 -0500

    Fix super installer

--- _scripts/super-installer.sh
+++ _scripts/super-installer.sh
@@ -68,6 +68,10 @@ brew cask install --appdir="/Applications" ${apps[@]}
 echo "Setting Git to use Atom as default editor"
 git config --global core.editor "atom -n -w"
 
+# Let's make the Terminal Great AGAIN!
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-wget https://gist.githubusercontent.com/rtablada/616652322eb096a0e476/raw/a5e595962a155583ff162a5940b3a65e971bab79/batcharge.py ~/bin
-wget https://gist.githubusercontent.com/rtablada/616652322eb096a0e476/raw/a5e595962a155583ff162a5940b3a65e971bab79/doubleend.zsh-theme ~/.oh-my-zsh/themes
+
+mkdir ~/bin
+
+wget https://gist.githubusercontent.com/rtablada/616652322eb096a0e476/raw/a5e595962a155583ff162a5940b3a65e971bab79/batcharge.py -O ~/bin/batcharge.py
+wget https://gist.githubusercontent.com/rtablada/616652322eb096a0e476/raw/a5e595962a155583ff162a5940b3a65e971bab79/doubleend.zsh-theme -O ~/.oh-my-zsh/themes/doubleend.zsh-theme
